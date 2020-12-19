41ec925290007f020756f170fe14e5beb999fdeb
fyan1fyan1 <57881075+fyan1fyan1@users.noreply.github.com>
2020-12-19 00:09:30 -0600

    Update speechRec_exp.py

--- speechRec_exp.py
+++ speechRec_exp.py
@@ -4,12 +4,10 @@ import speech_recognition as sr
 r = sr.Recognizer()  
 with sr.Microphone() as source:  
    print("Please wait. Calibrating microphone...")  
-   # listen for 5 seconds and create the ambient noise energy level  
    r.adjust_for_ambient_noise(source, duration=1)  
    print("Say something!") 
    audio = r.listen(source)  
    
- # recognize speech using Sphinx  
 try:  
    print("Sphinx thinks you said '" + r.recognize_sphinx(audio) + "'")  
 except sr.UnknownValueError:  
